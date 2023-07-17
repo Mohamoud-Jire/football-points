9b7fcb6f2a22439805c4be5632088d59fa9a8b71
Mohamoud-Jire <amoh54932@gmail.com>
2023-07-17 19:22:29 +0300

    Answered

--- challenge.js
+++ challenge.js
@@ -1,2 +1,6 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+function calculatePoints(wins, draws, loses) {
+  console.log((wins * 3) + (draws * 1) + (loses * 0));
+}
+calculatePoints(2, 3, 1);
